95399df8c4d6505776946114250786888bc1e5c7
Azhar <32222910+Azr-Erzr@users.noreply.github.com>
2026-03-17 01:33:04 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -15,11 +16,11 @@ export default function App() {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/listings" element={<Listings />} />
-            <Route path="/profiles" element={<Profiles />} />
-            <Route path="/listing/:id" element={<ListingDetail />} />
-            <Route path="/profile/:id" element={<ProfileDetail />} />
+            <Route path="/"              element={<Home />} />
+            <Route path="/listings"      element={<Listings />} />
+            <Route path="/profiles"      element={<Profiles />} />
+            <Route path="/listings/:id"  element={<ListingDetail />} />
+            <Route path="/profiles/:id"  element={<ProfileDetail />} />
           </Routes>
         </main>
         <Footer />
