f86c122e2c1d9117fe3f2757fb828ddb24f2399e
Azhar <32222910+Azr-Erzr@users.noreply.github.com>
2026-03-17 03:01:32 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Listings from "./pages/Listings";
 import Profiles from "./pages/Profiles";
 import ListingDetail from "./pages/ListingDetail";
 import ProfileDetail from "./pages/ProfileDetail";
+import ListHome from "./pages/ListHome";
+import CreateProfile from "./pages/CreateProfile";
+import About from "./pages/About";
+import HowItWorks from "./pages/HowItWorks";
 
 export default function App() {
   return (
@@ -16,11 +20,15 @@ export default function App() {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <Route path="/"              element={<Home />} />
-            <Route path="/listings"      element={<Listings />} />
-            <Route path="/profiles"      element={<Profiles />} />
-            <Route path="/listings/:id"  element={<ListingDetail />} />
-            <Route path="/profiles/:id"  element={<ProfileDetail />} />
+            <Route path="/"               element={<Home />} />
+            <Route path="/listings"       element={<Listings />} />
+            <Route path="/profiles"       element={<Profiles />} />
+            <Route path="/listings/:id"   element={<ListingDetail />} />
+            <Route path="/profiles/:id"   element={<ProfileDetail />} />
+            <Route path="/list-home"      element={<ListHome />} />
+            <Route path="/create-profile" element={<CreateProfile />} />
+            <Route path="/about"          element={<About />} />
+            <Route path="/how-it-works"   element={<HowItWorks />} />
           </Routes>
         </main>
         <Footer />
