450a4433c4df404e675d0e6bee915f7f40f987f5
Azhar <32222910+Azr-Erzr@users.noreply.github.com>
2026-03-17 03:25:36 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastProvider } from "./components/Toast";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -12,27 +13,31 @@ import ListHome from "./pages/ListHome";
 import CreateProfile from "./pages/CreateProfile";
 import About from "./pages/About";
 import HowItWorks from "./pages/HowItWorks";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/"               element={<Home />} />
-            <Route path="/listings"       element={<Listings />} />
-            <Route path="/profiles"       element={<Profiles />} />
-            <Route path="/listings/:id"   element={<ListingDetail />} />
-            <Route path="/profiles/:id"   element={<ProfileDetail />} />
-            <Route path="/list-home"      element={<ListHome />} />
-            <Route path="/create-profile" element={<CreateProfile />} />
-            <Route path="/about"          element={<About />} />
-            <Route path="/how-it-works"   element={<HowItWorks />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <ToastProvider>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/"               element={<Home />} />
+              <Route path="/listings"       element={<Listings />} />
+              <Route path="/profiles"       element={<Profiles />} />
+              <Route path="/listings/:id"   element={<ListingDetail />} />
+              <Route path="/profiles/:id"   element={<ProfileDetail />} />
+              <Route path="/list-home"      element={<ListHome />} />
+              <Route path="/create-profile" element={<CreateProfile />} />
+              <Route path="/about"          element={<About />} />
+              <Route path="/how-it-works"   element={<HowItWorks />} />
+              <Route path="*"               element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </ToastProvider>
     </Router>
   );
 }
